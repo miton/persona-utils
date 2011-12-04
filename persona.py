e519b71f6827def976d2161c2cfe06f6a68701c1
miton <mitongasukiyo@gmail.com>
2011-12-04 18:45:39 -0500

    add ids for personas, skills from data file

--- persona.py
+++ persona.py
@@ -203,60 +203,6 @@ for arcana in persona_by_arcana.keys():
   persona_by_arcana[arcana].sort(key=lambda i: i.base_level)
   #print map(lambda i: i.name, persona_by_arcana[arcana])
 
-def can_inherit(persona_type, skill_type):
-  #missing the types I don't care about right now
-  possibilities = {
-    'all'      : {'fire':  True, 'ice' :  True, 'elec' :  True, 'wind' :  True}
-    'status'   : {'fire':  True, 'ice' :  True, 'elec' :  True, 'wind' :  True}
-    'l&d'      : {'fire':  True, 'ice' :  True, 'elec' :  True, 'wind' :  True}
-    'recovery' : {'fire':  True, 'ice' :  True, 'elec' :  True, 'wind' :  True}
-    'slash'    : {'fire': False, 'ice' : False, 'elec' : False, 'wind' : False}
-    'strike'   : {'fire': False, 'ice' : False, 'elec' : False, 'wind' : False}
-    'pierce'   : {'fire': False, 'ice' : False, 'elec' : False, 'wind' : False}   
-    'fire'     : {'fire': False, 'ice' : False, 'elec' : False, 'wind' : False}
-    'ice'      : {'fire': False, 'ice' : False, 'elec' : False, 'wind' : False}
-    'elec'     : {'fire': False, 'ice' : False, 'elec' : False, 'wind' : False}
-    'wind'     : {'fire': False, 'ice' : False, 'elec' : False, 'wind' : False}
-    'light'    : {'fire':  True, 'ice' :  True, 'elec' :  True, 'wind' :  True}
-    'dark'     : {'fire':  True, 'ice' :  True, 'elec' :  True, 'wind' :  True}
-  }
-  return possibilities[persona_type][skill_type]
-
-def fusion_2(a, b):
-  result_level = 1 + floor((a.level + b.level) / 2.0)
-  result_arcana = fusion2_arcana[a.arcana][b.arcana]
-  
-  arcana_list = persona_by_arcana[result_arcana]
-  for i,persona in enumerate(arcana_list):
-    if persona.base_level >= result_level and not persona.special:
-      break
-
-  if a.arcana == b.arcana:
-    i -= 1
-  if persona == a  or persona == b:
-    i -= 1
-  #can this go negative?
-  return arcana_list[i]
-
-def fusion_3(a, b, c, this_order = False):
-  if not this_order:
-    a, b, c = sorted([a,b,c], key=lambda i: i.level * 100 + arcana_order[i.arcana]))
-  intermediate_arcana = fusion2_arcana[a.arcana][b.arcana]
-  
-  result_level = 5 + floor((a.level + b.level + c.level) / 3.0)
-  result_arcana = fusion3_arcana[intermediate_arcana][c.arcana]
-
-  arcana_list = persona_by_arcana[result_arcana]
-  found = False
-  for i,persona in enumerate(arcana_list):
-    if persona.base_level >= result_level and not persona.special:
-      found = True
-      break
-
-  if not found:
-    return None
-  return persona
-
 
   
   
